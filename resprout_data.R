466927ec5d0af269e969c1982e157404400a9e7e
skyeaustin <oskyeaustin@gmail.com>
2021-06-03 12:05:22 -0400

    added skye's mac

--- resprout_data.R
+++ resprout_data.R
@@ -4,14 +4,15 @@ library(tidyverse)
 library(dplyr)
 library(ggpubr)
 library(RColorBrewer)
-library(glmm)
+
 #set working directory
 setwd("~/SERC/ExperimentData_and_Code/SERC_REU2020") #Skye's desktop
-
+setwd("~/Documents/r_stuff/SERC/SERC_REU2020") #skye's macbook
 
 ###Resprout Data Code###
 file.choose()
-respt1 <- read.csv("C:\\Users\\Airsi\\OneDrive\\Documents\\SERC\\ExperimentData_and_Code\\SERC_REU2020\\resprout_combined_SAUS_11172020.csv")
+respt1 <- read.csv("C:\\Users\\Airsi\\OneDrive\\Documents\\SERC\\ExperimentData_and_Code\\SERC_REU2020\\resprout_combined_SAUS_11172020.csv") #skye's desktop
+respt1 <- read.csv("/Users/saus/Documents/r_stuff/SERC/SERC_REU2020/resprout_combined_SAUS_11172020.csv") #skye's mac
 ggqqplot(respt1$resprouts)#see if the data is normally distributed
 shapiro.test(respt1$resprouts)#test statistical significance
 
