dbbd90e14bc1bfabc7d25f8728e8ca234b6cc97a
rachbrenn <rach.brenneman@gmail.com>
2021-09-08 16:47:51 -0400

    two new practice graphs

--- data_exploration.R
+++ data_exploration.R
@@ -6,6 +6,47 @@ library(tidyverse)
 library(RColorBrewer)
 library(ggpubr)
 
+setwd("~/Documents/Data") #working directory
+
+biom1 <- read.csv("plant_biomass_all.csv") ### amy's computers ###
+
+# remove weird columns ##
+biom1.5 <- select(biom1, -starts_with("X"))
+
+## species combination ##
+biom2<- biom1.5 %>% 
+  mutate(combination = substring(pot_id, 7, 10))
+
+## hetero v. con specific ##
+biom3 <- biom2%>% 
+  mutate(mono_hetero = if_else(biom2$combination %in% c("EpEV", "CnEv", "CnEp"), "hetero", "mono"))
+
+## nitrogen treatment ##
+biom4 <- biom3 %>% 
+  mutate(nitrogen = substring(pot_id, 5,5))
+
+## make total biomass column ##
+biom5 <- biom4 %>% 
+  mutate(total_biomass = above_biomass_g+below_biomass_g)
+
+
+## as if something is numeric, character, factor, integer ## 
+is.numeric(biom5$above_biomass_g)
+
+## define columns as numeric ##
+biom5$above_biomass_g <- as.numeric(as.character(biom5$above_biomass_g))
+biom5$below_biomass_g <- as.numeric(as.character(biom5$below_biomass_g))
+biom5$total_biomass <- as.numeric(as.character(biom5$total_biomass))
+
+## remove dead plants ##
+biom6 <- na.omit(biom5)
+
+## create yard column ##
+biom6$yard = substring(biom6$pot_id, 12,14)
+
+
+
+
 ## bar graph summary statistics function from Kim ##
 barGraphStats(data=, variable="", byFactorNames=c(""))
 barGraphStats <- function(data, variable, byFactorNames) {
@@ -23,13 +64,21 @@ barGraphStats <- function(data, variable, byFactorNames) {
   finalSummaryStats <- merge(preSummaryStats, sd, by=byFactorNames)
   finalSummaryStats$se <- finalSummaryStats$sd / sqrt(finalSummaryStats$N)
   return(finalSummaryStats)
+}
   
-g1 <- ggplot(data = barGraphStats(data = damage2, variable = "Percent_damage", byFactorNames = c("Area_type")), aes(x=Area_type, y=mean, fill=Area_type)) +
+g1 <- ggplot(data = barGraphStats(data = biom6, variable = "total_biomass", byFactorNames = c("nitrogen")), aes(x=nitrogen, y=mean, fill=nitrogen)) +
     geom_bar(stat='identity', position=position_dodge(), width=0.8) +
     geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width = 0.2, position = position_dodge(0.9)) +
     annotate("text", x = 1, y = 2.7 , label = "a", size = 8)+
     annotate("text", x = 2, y = 2.7 , label = "a", size = 8)+
     annotate("text", x = 3, y = 1.75, label = "b", size = 8) +
-    scale_fill_manual(values = EBpalette1) + theme(legend.position = "none") + xlab("Area Type") + ylab("Percent Damage")
+    scale_fill_brewer(palette = "Set1") + theme(legend.position = "none") + xlab("Nitrogen") + ylab("Total Biomass")
   
 g1 + theme(panel.background = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), text = element_text(size = 24),axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)), axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)), legend.position = "none")
+
+g2 <- ggplot(data = barGraphStats(data = biom6, variable = "total_biomass", byFactorNames = c("nitrogen", "combination")), aes(x=nitrogen, y=mean, fill=combination)) +
+  geom_bar(stat='identity', position=position_dodge(), width=0.8) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width = 0.2, position = position_dodge(0.9)) +
+  scale_fill_brewer(palette = "Set1") + xlab("Nitrogen") + ylab("Total Biomass")
+
+g2 + theme(panel.background = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), text = element_text(size = 24),axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)), axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)), legend.position = "none")
